2026fef7209b53aaf07a96d2079fa9a42acd7d22
kauelima21 <kaueslim@outlook.com>
2024-03-06 21:14:00 -0300

    go to remote repo

---
+++ InsertProduct/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+func InsertProduct(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var product Product
+	err := json.Unmarshal([]byte(request.Body), &product)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
+	}
+
+	product.ID = uuid.New().String()
+	sess := session.Must(session.NewSession())
+	service := dynamodb.New(sess)
+
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String("Products"),
+		Item: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(product.ID),
+			},
+			"name": {
+				S: aws.String(product.Name),
+			},
+			"price": {
+				N: aws.String(strconv.Itoa(product.Price)),
+			},
+		},
+	}
+
+	_, err = service.PutItem(input)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
+	}
+
+	body, err := json.Marshal(product)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 201,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(body),
+	}, nil
+}
+
+func main() {
+	lambda.Start(InsertProduct)
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/kauelima21/go-serverless
+
+go 1.21.6
+
+require (
+	github.com/aws/aws-lambda-go v1.46.0
+	github.com/aws/aws-sdk-go v1.50.33
+	github.com/google/uuid v1.6.0
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
